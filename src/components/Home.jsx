e828f506ed42f01cfa82a4641261a291444f426c
RenaudVernageau <renaudvrng@gmail.com>
2022-10-14 16:59:07 +0200

    fixed css

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,6 @@
 export default function Home({}) {
     return <>
-            <h1>Bienvenue dans mon pokédex</h1>
+            <h1 className="home">Bienvenue dans mon pokédex</h1>
             <figure>
                 <img src="./pokemon-pokeball.gif" alt="Pokeball"/>
             </figure>
--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -11,20 +11,20 @@ export default function Pokemon({ pokemon }) {
           <img src={pokemon.sprites.front_shiny} alt={pokemon.name} />
           <img src={pokemon.sprites.back_shiny} alt={pokemon.name} />
           <span className="main__features">
-            <p>Hauteur: {pokemon.height}"</p>
-            <p>Poids: {pokemon.weight} lbs</p>
-            <p>Expérience: {pokemon.base_experience} xp</p>
+            <p>Hauteur: <b>{pokemon.height}</b>"</p>
+            <p>Poids: <b>{pokemon.weight}</b> lbs</p>
+            <p>Expérience: <b>{pokemon.base_experience}</b> xp</p>
           </span>
           <p>Types: </p>
-          <p>{pokemon.types.map((type,i) => {
+          <p className="specs">{pokemon.types.map((type,i) => {
               return <span key={i} className="abilities">{type.type.name} </span>
           })}</p>
           <p>Capacités: </p>
-          <p>{pokemon.abilities.map((ability,i) => {
+          <p className="specs">{pokemon.abilities.map((ability,i) => {
               return <span key={i} className="abilities">{ability.ability.name} </span>
           })}</p>
           <p>Statistiques: </p>
-          <p>{pokemon.stats.map((stat,i) => {
+          <p className="specs">{pokemon.stats.map((stat,i) => {
               return <span key={i} className="abilities">{stat.stat.name} </span>
           })}</p>
         </li>
